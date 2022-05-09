b837a69a1094f50dfac293e6b1278e4c740511c9
Joseph Kowalski <jkjr25@gmail.com>
2022-05-09 19:39:45 -0400

    Added banner image

--- streamlit.py
+++ streamlit.py
@@ -1,21 +1,82 @@
 import streamlit as st
 import pandas as pd
-
-# df = pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-#     })
-#
-# option = st.selectbox(
-#     'Which number do you like best?',
-#      df['first column'])
-#
-# 'You selected: ', option
+from PIL import Image
 
 df = pd.read_csv('games_of_all_time.csv')
-df2 = df1.drop(['url'], 1)
+df2 = df.drop(['url', 'developer'], 1)
+df2.fillna('No Data', inplace=True)
+df2['genre'] = df2['genre'].str.replace('[', '', regex=True)
+df2['genre'] = df2['genre'].str.replace(']', '', regex=True)
+df2['genre'] = df2['genre'].str.replace("'", '', regex=True)
+df2['genre'] = df2['genre'].str.replace("'", '', regex=True)
+df2['platform'] = df2['platform'].str.replace('[', '', regex=True)
+df2['platform'] = df2['platform'].str.replace(']', '', regex=True)
+df2['platform'] = df2['platform'].str.replace("'", '', regex=True)
+df2['platform'] = df2['platform'].str.replace("'", '', regex=True)
 
-option = st.selectbox(
-    'What genre would you like to find games for?',
-    df2[]
-)
+image = Image.open('Genre games4u.png')
+st.image(image, width=1000)
+
+def recommender(searched_genre):
+    games = df2[df2['genre'].str.contains(searched_genre, case=False)]
+    # games = temp.reset_index()
+    st.dataframe(games.query('((meta_score + user_score) / 2) > 50.0').sample(10).style.format({'user_score': '{:.2f}', 'meta_score': '{:.2f}'}))
+    return games.query('((meta_score + user_score) / 2) > 50.0').sample(10)
+
+
+searched_genre = st.selectbox('Genre Select', ['Beat-Em-Up',
+                              '2D',
+                              '3D',
+                              'Action',
+                              'Action Adventure',
+                              'Action RPG',
+                              'Adventure',
+                              'Arcade',
+                              'Breeding/Constructing',
+                              'Card Battle',
+                              'City Building',
+                              'Compilation',
+                              'Console-style RPG',
+                              'Dancing',
+                              'Driving',
+                              'Fantasy',
+                              'Fighting',
+                              'First-Person',
+                              'Futuristic',
+                              'GT / Street',
+                              'General',
+                              'Government',
+                              'Historic',
+                              'Horror',
+                              'Ice Hockey',
+                              'Japanese-Style',
+                              'Massively Multiplayer',
+                              'Massively Multiplayer Online',
+                              'Military',
+                              'Miscellaneous',
+                              'Modern',
+                              'Music',
+                              'No Data',
+                              'Olympic Sports',
+                              'Other',
+                              'PC-style RPG',
+                              'Party',
+                              'Pinball',
+                              'Platformer',
+                              'Puzzle',
+                              'Racing',
+                              'Rally / Offroad',
+                              'Role-Playing',
+                              'Sci-Fi',
+                              'Scrolling',
+                              'Shooter',
+                              'Simulation',
+                              'Sports',
+                              'Strategy',
+                              'Third-Person',
+                              'Traditional',
+                              'Turn-Based',
+                              'Tycoon',
+                              'WWI'])
+
+recommender(searched_genre)
